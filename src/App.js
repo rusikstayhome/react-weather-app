cab86a81489fd7e6afe6187bddb21e51c538a59c
rusikstayhome <alenrus32@gmail.com>
2022-07-11 15:35:52 +0300

    added GitHud repo url

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-    Weather app
+      Weather app
+      <footer>
+        This project is {' '}
+        <a href='https://github.com/svrmoar/weather-app-react' target='_blank' rel="noreferrer">open-sourced</a>
+      </footer>
     </div>
   );
 }
