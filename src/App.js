ec39aeba07312b9bab2adb39f336fa37287b593b
rusikstayhome <alenrus32@gmail.com>
2022-07-11 15:48:41 +0300

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather app
-      <footer>
-        This project is {' '}
-        <a href='https://github.com/svrmoar/weather-app-react' target='_blank' rel="noreferrer">open-sourced</a>
-      </footer>
+      <div className='container'>
+        Weather app
+        <footer>
+          This project is {' '}
+          <a href='https://github.com/svrmoar/weather-app-react' target='_blank' rel="noreferrer">open-sourced</a>
+        </footer>
+      </div>
     </div>
   );
 }
