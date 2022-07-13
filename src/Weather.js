237793866b7200aabf1e9c160e94ddf7766ee393
rusikstayhome <alenrus32@gmail.com>
2022-07-13 11:30:02 +0300

    added the date to the Weather component

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import './Weather.css'
+import './Weather.css';
+import FormatedDate from './FormatedDate';
 
 export default function Weather(props) {
     const [weatherData, setWeatherData] = useState({ ready: false });
@@ -15,7 +16,7 @@ export default function Weather(props) {
             city: response.data.name,
             description: response.data.weather[0].description,
             iconUrl: `https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png`,
-            date: 'Wednesday 7:00'
+            date: new Date(response.data.dt * 1000)
         });
     }
 
@@ -36,7 +37,9 @@ export default function Weather(props) {
                 </form>
                 <h1>{weatherData.city}</h1>
                 <ul>
-                    <li>{weatherData.date}</li>
+                    <li>
+                        <FormatedDate date={weatherData.date} />
+                    </li>
                     <li className='text-capitalize'>{weatherData.description}</li>
                 </ul>
                 <div className="row mt-3">
@@ -55,7 +58,6 @@ export default function Weather(props) {
         )
     } else {
         const apiKey = 'b4e5671cab86d3aadb0d688766b69716';
-        let city = 'London';
         let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
         axios.get(apiUrl).then(handleResponse);
 
